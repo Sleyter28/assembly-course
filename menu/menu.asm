2553b8a14cb98bfca8197bf1947d84980fd15e09
Sleyter Angulo <sley9468@hotmail.com>
2024-09-04 18:23:45 -0600

    Refactored main menu

--- menu/menu.asm
+++ menu/menu.asm
@@ -12,32 +12,45 @@ section .text                                   ;Code segment
     global _start
 
 _start:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, userMsg
-    mov edx, lenUserMsg
-    int 80h
 
-    ;Leer y almacenar el valor leido
-    mov eax, 3
-    mov ebx, 2
-    mov ecx, inputNumber
-    mov edx, 5                          ;5 bytes (numeric, 1 para el signo) de la informacion
-    int 80h
+    printInputMsg:
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, userMsg
+        mov edx, lenUserMsg
+        int 80h
+    
+    getValue:
+        ;Leer y almacenar el valor leido
+        mov eax, 3
+        mov ebx, 2
+        mov ecx, inputNumber
+        mov edx, 5                          ;5 bytes (numeric, 1 para el signo) de la informacion
+        int 80h
+    
+    printMsgValue:
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, displayMsg
+        mov edx, lenDisplayMsg
+        int 80h
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, displayMsg
-    mov edx, lenDisplayMsg
-    int 80h
+    printValue:
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, inputNumber
+        mov edx, 5
+        int 80h
 
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, inputNumber
-    mov edx, 5
-    int 80h
+    _exit:
+        mov eax, 1
+        mov ebx, 0
+        int 80h
+    
 
+    JMP printInputMsg
+    JMP getValue
+    JMP printMsgValue
+    JMP printValue
     ;Terminar ejecucion
-    mov eax, 1
-    mov ebx, 0
-    int 80h
+    JMP _exit
